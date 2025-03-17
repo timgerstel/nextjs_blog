c2d7bb9a5fadc0d3e9948ba0117f8684030e4127
Timothy Gerstel <tim.gerstel@gmail.com>
2025-03-17 01:38:14 -0400

    Update page to accomdoate 20 item pagination. Add more peer testimonials
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- tg-blog/src/app/page.tsx
+++ tg-blog/src/app/page.tsx
@@ -5,7 +5,7 @@ import { Meteors } from '@/components/ui/meteors';
 import { DataTable } from '@/components/ui/data-table';
 import { columns } from '@/components/ui/data-table.d';
 import { Boxes } from '@/components/ui/background-boxes';
-import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+import { HardHat, Construction } from 'lucide-react';
 
 function MeteorCard(props: any){
   return (
@@ -21,7 +21,7 @@ function MeteorCard(props: any){
               {props.desc}
             </p>
   
-            <button className="border px-3 py-1 rounded-lg  border-gray-500 text-slate-700 dark:text-gray-300 text-xs hover:bg-slate-700">
+            <button className="border px-3 py-1 rounded-lg  border-gray-500 text-slate-700 dark:text-gray-300 text-xs hover:bg-slate-300 dark:hover:bg-slate-700">
               tru 👍
             </button>
   
@@ -47,44 +47,50 @@ export default async function Home() {
       <div className="tg-app-main-container w-full font-(family-name:--Inter)">
         <MenuBar />
         <main className="flex flex-col items-center
-        h-[calc(100vh)]
+        h-[calc(100vh-50px)]
         ml-5 md:ml-10
         mr-5 md:mr-10
         border-l border-r border-dashed border-[rgba(0,132,255,0.1)]
         overflow-hidden z-10">
+          <h1 className="font-extrabold flex flex-row my-5"><Construction />&nbsp;UNDER CONSTRUCTION&nbsp;<Construction /></h1>
           <div className="w-full 
             pl-5 md:pl-10
             pr-5 md:pr-10
-          absolute flex flex-col items-center overflow-hidden z-30">
+          absolute flex flex-col items-center overflow-hidden z-30 top-20">
             <DataTable columns={columns} data={postTitles.map((post: any, ind: any) => {
               const parts = post.split("---");
               return {id: ind, title: parts[1], desc: parts[2], date: parts[0], url: encodeURIComponent(post)}
             })}/>
           </div>
 
-          <div className="h-[1200px] md:h-[700px] absolute w-full top-[650px]
+          <div className="h-[1700px] lg:h-[900px] absolute w-full top-[1200px]
             overflow-hidden">
-              <div className="dark:bg-[rgba(0,0,0,0.03)]
-              h-[1200px] md:h-[700px]
+              <div className="dark:bg-[rgba(0,0,0,0.02)]
+              h-[1700px] lg:h-[900px]
               ml-5 md:ml-10
               mr-5 md:mr-10
               bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex flex-col items-center justify-center -z-10" />
             <div className="absolute inset-0 w-full
-            h-full dark:bg-[rgba(0,0,0,0.03)] bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] pointer-events-none" />
+            h-full dark:bg-[rgba(0,0,0,0.02)] bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] pointer-events-none" />
       
             {/* <Boxes className="sm:hidden md:hidden lg:hidden xl:hidden 2xl:flex"/> */}
           </div>
           
-          <div className="flex flex-col items-center absolute top-[750px]">
-            <h1 className="text-3xl font-extrabold -z-10">Loved by peers.</h1>
-            <div className="flex flex-col sm:flex-row">
-              <div className="flex flex-col py-5 px-5">
-                <MeteorCard title="Architect" desc="Did someone gave Stevie Wonder a regular computer keyboard?" />
+          <div className="flex flex-col items-center absolute top-[1300px]">
+            <h1 className="text-3xl font-extrabold -z-10 mb-10">Loved by peers.</h1>
+            <div className="flex flex-col lg:flex-row">
+              <div className="flex flex-col lg:pt-16 px-5">
+                <MeteorCard title="Architect" desc="Did someone give Stevie Wonder a regular computer keyboard?" />
                 <MeteorCard title="Test Engineer" desc="Idt he knows what a unit test is lmao." />
               </div>
               <div className="flex flex-col px-5">
                 <MeteorCard title="Manager" desc="this guy sucks" />
                 <MeteorCard title="Scrum Master" desc="It's a miracle he remembers to breathe, honestly." />
+                <MeteorCard title="College Advisor" desc="My lawyer advised me not to comment if I want to keep my job." />
+              </div>
+              <div className="flex flex-col lg:mt-16 px-5">
+                <MeteorCard title="Ex Wife" desc="He literally gambled the down payment for our first house on meme coins." />
+                <MeteorCard title="Dog" desc="🐶 woof" />
               </div>
             </div>
           </div>
